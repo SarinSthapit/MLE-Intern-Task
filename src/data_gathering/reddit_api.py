277346fc8d9a1d853ef841b202e144b868081f86
SarinSthapit <sarinsthapit.2001@gmail.com>
2024-10-07 17:29:55 +0545

    Function to create and return a Reddit instance using PRAW

---
+++ src/data_gathering/reddit_api.py
@@ -0,0 +1,13 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def reddit_instance():
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        user_agent=os.getenv("REDDIT_USER_AGENT")
+    )
+    return reddit
